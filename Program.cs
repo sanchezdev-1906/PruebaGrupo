2c30153c584f6e71ae22cccb8a12774d9d4dc0c7
Joel Sanchez <joelalejandro.development@gmail.com>
2023-05-05 11:31:51 -0400

    funcion para calcular cuadraticas

--- Program.cs
+++ Program.cs
@@ -1,6 +1,24 @@
-void suma(){
-  return 2;
-}
-void resta(){
-    return "pito";
+using System;
+namespace myConsole
+{
+  class Program
+  {
+    static void CalcularCuadratica(double a, double b, double c, out double x1, out double x2)
+    {
+      double sq = Math.Sqrt(Math.Pow(b, 2) + 4 * a * c);
+      if ((a == 0) || (sq < 0))
+      {
+        x1 = 0;
+        x2 = 0;
+      }
+      else
+      {
+        x1 = (-b + sq) / (2 * a);
+        x2 = (-b - sq) / (2 * a);
+      }
+    }
+    static void Main()
+    {
+    }
+  }
 }
